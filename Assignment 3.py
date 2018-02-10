85cefe6415cbb2e9d8947bb2220927bcd083f392
sripadh4 <35394080+sripadh4@users.noreply.github.com>
2018-02-10 10:18:02 +0530

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[1]:
+
+Red_Balls = 4
+Black_Balls = 6
+N_Factorial = ((10 * 9)/(2))
+Prob_Two_RB = (((4 * 3) / (2)) / N_Factorial)
+Prob_Two_BB = (((6 * 5) / (2)) / N_Factorial)
+Prob_One_RB_BB = ((4 * 6) / N_Factorial)
+print (Prob_Two_RB)
+print (Prob_Two_BB)
+print (Prob_One_RB_BB)
+
